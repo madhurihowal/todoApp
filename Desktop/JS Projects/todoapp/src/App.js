0f10a113a5aa63444e0c996cb99e5a09ec3f36c7
Madhuri Howal github <madhurihowal2020@gmail.com>
2024-01-30 18:25:56 +0530

    firstcommit

---
+++ Desktop/JS Projects/todoapp/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [activity, setActivity] = useState("");
+  const [listData, setlistData] = useState([]);
+
+  function addActivity() {
+    //  setlistData([...listData,activity])
+    //  console.log(listData)    ---> This is the asynchronous printing on console so this is not proper way
+    setlistData((listData) =>
+    {
+      const updatedList = [...listData, activity]
+      console.log(updatedList)
+      setActivity("");
+      return updatedList
+    })
+  }
+
+// REMOVE BUTTON
+  function removeActivity(i)
+  {
+    const updatedListData = listData.filter((ele,id)=>{
+      return i!=id;
+    })
+  setlistData(updatedListData)
+
+  }
+
+// REMOVE ALL BUTTON
+  function removeAll(){
+    setlistData("")
+  }
+  return (
+    <>
+      <div className='container'>
+        <div className="header">TODO LIST</div>
+        <input type="text" placeholder='Add Activity' value={activity} onChange={(e) => setActivity(e.target.value)} />
+
+        <button onClick={addActivity}>Add</button>
+        <p className='list-heading'>Here is your list : {")"}</p>
+        {listData != [] && listData.map((data, i) => {
+          return (
+            <>
+              <p key={i}>
+               <div className='listData'>{data}</div>
+               <div className="btn-position">               
+               <button onClick={()=>removeActivity(i)}>Remove</button>
+              </div>
+               </p>
+            </>
+          )
+        })}
+        {listData.length>=1 && <button onClick={removeAll}>Remove All</button>}
+    </div>
+    </>
+  )
+}
+
+export default App
